980a56ad0af30dc461e1da0413f43ea73be545ca
Cole Fortner <colefortner@gmail.com>
2022-02-09 23:54:08 -0500

    Switch to a state managed/controlled form.  All inputs working wired and working properly except for file upload input (avatar)

--- src/users/pages/Auth.tsx
+++ src/users/pages/Auth.tsx
@@ -1,39 +1,69 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 
 const Auth: React.FC = () => {
-  const usernameInputRef = React.useRef<HTMLInputElement>(null);
-  const emailInputRef = React.useRef<HTMLInputElement>(null);
-  const passwordInputRef = React.useRef<HTMLInputElement>(null);
-  const confirmPasswordInputRef = React.useRef<HTMLInputElement>(null);
+  // const usernameInputRef = useRef<HTMLInputElement>(null);
+  // const avatarInputRef = useRef<HTMLInputElement>(null);
+  // const emailInputRef = useRef<HTMLInputElement>(null);
+  // const passwordInputRef = useRef<HTMLInputElement>(null);
+  // const confirmPasswordInputRef = React.useRef<HTMLInputElement>(null);
 
   const [loginMode, setLoginMode] = useState(false);
+  // const [pickedFile, setPickedFile] = useState();
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const avatarChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) {
+      return;
+    }
+    // setPickedFile(event.target.files[0] );
+    console.log(event.target.files[0]);
+    // console.log(pickedFile);
+  };
 
   const changeModeHandler = () => {
     setLoginMode(!loginMode);
   };
 
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  };
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
-    const email = emailInputRef.current!.value;
-    const password = passwordInputRef.current!.value;
-
     let data;
-    if (loginMode) {
-      data = { email, password };
-    } else {
-      const username = usernameInputRef.current!.value;
-      const confirmPassword = confirmPasswordInputRef.current!.value;
 
-      data = { username, email, password, confirmPassword };
+    if (loginMode) {
+      data = { email: formData.email, password: formData.password };
+    } else {
+      data = formData;
     }
+
     console.log(data);
+
+    setFormData({
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
   };
 
   return (
     <>
       <h2>{loginMode ? "Login" : "Signup"}</h2>
-      {/* <form onSubmit={formSubmitHandler}> */}
       <form onSubmit={submitHandler}>
         {!loginMode && (
           <div>
@@ -42,7 +72,25 @@ const Auth: React.FC = () => {
               id="username"
               type="text"
               placeholder="username"
-              ref={usernameInputRef}
+              name="username"
+              onChange={changeHandler}
+              value={formData.username}
+              required
+            />
+          </div>
+        )}
+        {!loginMode && (
+          <div>
+            <label htmlFor="avatar">Upload Avatar</label>
+            <input
+              id="avatar"
+              type="file"
+              multiple={false}
+              accept=".jpg,.png,.jpeg"
+              name="avatar"
+              // placeholder="confirm password"
+              onChange={avatarChangeHandler}
+              // value={pickedFile.avatar}
               required
             />
           </div>
@@ -53,7 +101,9 @@ const Auth: React.FC = () => {
             id="email"
             type="email"
             placeholder="email"
-            ref={emailInputRef}
+            name="email"
+            onChange={changeHandler}
+            value={formData.email}
             required
           />
         </div>
@@ -63,7 +113,9 @@ const Auth: React.FC = () => {
             id="password"
             type="password"
             placeholder="password"
-            ref={passwordInputRef}
+            name="password"
+            onChange={changeHandler}
+            value={formData.password}
             required
           />
         </div>
@@ -74,7 +126,9 @@ const Auth: React.FC = () => {
               id="confirm-password"
               type="password"
               placeholder="confirm password"
-              ref={confirmPasswordInputRef}
+              name="confirmPassword"
+              onChange={changeHandler}
+              value={formData.confirmPassword}
               required
             />
           </div>
