c82a8638f761cea2c95e3387d278aecb1fa8aecd
Cole Fortner <colefortner@gmail.com>
2022-02-22 17:51:11 -0500

    Add a delete button for each comment, that allows a user to delete that individual comment (based on the comment id)

--- src/shared/components/comments/Comment.tsx
+++ src/shared/components/comments/Comment.tsx
@@ -8,11 +8,20 @@ interface CommentProps {
 }
 
 const Comment: React.FC<CommentProps> = (props) => {
+  const deleteHandler = () => {
+    fetch(`http://localhost:5050/comments/${props.id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <li>
       <h2>
         Comment Number: {props.id} Comment: {props.review}
       </h2>
+      <button onClick={deleteHandler}>Delete</button>
     </li>
   );
 };
