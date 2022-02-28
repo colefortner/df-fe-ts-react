c3a4aad9a4fb8a26855c8a31b9f4f7c4a75e9dc0
Cole Fortner <colefortner@gmail.com>
2022-02-28 00:38:59 -0500

    Add button and delete route that allows a user to delete a business from their dashboard/saved businesses

--- src/businesses/components/Business.tsx
+++ src/businesses/components/Business.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Rating from "../../shared/components/UIElements/Rating";
+import { AuthContext } from "../../shared/context/auth-context";
 
 interface BusinessProps {
   key: string;
@@ -15,6 +16,40 @@ interface BusinessProps {
 }
 
 const Business: React.FC<BusinessProps> = (props) => {
+  const auth = useContext(AuthContext);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    fetch("http://localhost:5050/users/save-business", {
+      method: "POST",
+      body: JSON.stringify({
+        businessId: props.businessId,
+        userId: auth.userId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
+  const deleteHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    fetch("http://localhost:5050/users/delete-business/1", {
+      method: "DELETE",
+      body: JSON.stringify({
+        businessId: props.businessId,
+        userId: auth.userId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <li>
       <Link to={`/${props.businessId}/businesses`} state={{ cardData: props }}>
@@ -23,6 +58,8 @@ const Business: React.FC<BusinessProps> = (props) => {
         <img src={props.image} alt={props.name} style={{ width: 200 }} />
         <Rating rating={props.rating} />
       </Link>
+      <button onClick={submitHandler}>Add Business to Dashboard</button>
+      <button onClick={deleteHandler}>Delete Business From Dashboard</button>
     </li>
   );
 };
