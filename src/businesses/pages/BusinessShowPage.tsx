79b7312a1ded66ff5030d2bf9b9c0ec2eb9086b0
Cole Fortner <colefortner@gmail.com>
2022-03-03 15:47:49 -0500

    Move responsibility for rendering the comment form from BusinessShowPage.tsx to Comments.tsx add condition to check if the user has already made a comment and will not render a comment form if the user is not logged in or if the user has already has a comment attributed to the business

--- src/businesses/pages/BusinessShowPage.tsx
+++ src/businesses/pages/BusinessShowPage.tsx
@@ -1,11 +1,9 @@
 import React, { useContext } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import Rating from "../../shared/components/UIElements/Rating";
-import CommentForm from "../../shared/components/comments/ComentForm";
 import Comments from "../../shared/components/comments/Comments";
 import RatingForm from "../../shared/components/comments/RatingForm";
 import Map from "../../shared/components/UIElements/Maps/Map";
-import { AuthContext } from "../../shared/context/auth-context";
 
 interface Business {
   cardData: {
@@ -30,9 +28,7 @@ const BusinessShowPage: React.FC = () => {
   const location = useLocation();
   const state = location.state as Business;
   const { cardData } = state;
-  const auth = useContext(AuthContext);
 
-  // console.log(cardData.businessId);
   return (
     <>
       <p>{params.businessId}</p>
@@ -40,7 +36,6 @@ const BusinessShowPage: React.FC = () => {
       <img src={cardData.image} alt={cardData.name} style={{ width: 200 }} />
       <Rating rating={cardData.rating} />
       <Comments businessId={cardData.businessId} />
-      {auth.isLoggedIn && <CommentForm businessId={params.businessId} />}
       <RatingForm />
       {/* <div style={{ width: "400px", height: "400px" }}>
         <Map center={cardData.location} zoom={10} />
--- src/shared/components/comments/Comments.tsx
+++ src/shared/components/comments/Comments.tsx
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import CommentsList from "./CommentsList";
+import { AuthContext } from "../../context/auth-context";
+import CommentForm from "./ComentForm";
 
 interface CommentsProps {
   businessId: string | undefined;
 }
 
 const Comments: React.FC<CommentsProps> = (props) => {
-  const [commentsData, setCommentsData] = useState([]);
+  const auth = useContext(AuthContext);
+  const [commentsData, setCommentsData] = useState<any[]>([]);
 
   useEffect(() => {
     fetch(`http://localhost:5050/comments/${props.businessId}`)
@@ -18,7 +21,18 @@ const Comments: React.FC<CommentsProps> = (props) => {
       });
   }, []);
 
-  return <CommentsList businessId={props.businessId} comments={commentsData} />;
+  const hasCommented = commentsData.some(
+    (comment) => comment.userId === auth.userId
+  );
+
+  return (
+    <>
+      <CommentsList businessId={props.businessId} comments={commentsData} />
+      {hasCommented === false && auth.isLoggedIn && (
+        <CommentForm businessId={props.businessId} />
+      )}
+    </>
+  );
 };
 
 export default Comments;
