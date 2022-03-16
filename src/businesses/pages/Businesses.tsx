b81167396a3b524293961f6bafa76435aac4a753
Cole Fortner <colefortner@gmail.com>
2022-03-15 23:58:50 -0400

    Add logic to create a list of all promotions from all businesses and render them on the landing page

--- src/businesses/pages/Businesses.tsx
+++ src/businesses/pages/Businesses.tsx
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 import BusinessList from "../components/BusinessList";
+import PromotionsList from "../../shared/components/promotions/PromotionsList";
 
 const Businesses: React.FC = () => {
   const [businessesData, setBusinessesData] = useState([]);
+  const [promotionsData, setPromotionsData] = useState<any[]>([]);
+
+  const PromotionsContainer = styled.div`
+    list-style: none;
+    text-align: center;
+    height: 500px;
+    width: 425px;
+    overflow-y: auto;
+  `;
 
   useEffect(() => {
     fetch("http://localhost:5050/businesses")
@@ -14,15 +25,32 @@ const Businesses: React.FC = () => {
       });
   }, []);
 
+  const getPromotionsData = (data: any) => {
+    let newArr: any = [];
+
+    data.forEach((item: { promotions: any[] }) =>
+      newArr.push(...item.promotions)
+    );
+    setPromotionsData(newArr);
+    return newArr;
+  };
   // console.log(businessesData[1].);
+  useEffect(() => {
+    getPromotionsData(businessesData);
+  }, [businessesData]);
 
   return (
-    <BusinessList
-      businesses={businessesData}
-      dashboard={false}
-      landing={true}
-      removeBusinessFromDashboard={(id: any) => {}}
-    />
+    <>
+      <BusinessList
+        businesses={businessesData}
+        dashboard={false}
+        landing={true}
+        removeBusinessFromDashboard={(id: any) => {}}
+      />
+      <PromotionsContainer>
+        <PromotionsList promotions={promotionsData} />
+      </PromotionsContainer>
+    </>
   );
 };
 
