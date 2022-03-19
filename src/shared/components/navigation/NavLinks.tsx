493f66d4f250d3b7bedb064c5579a2e5ab2f8fb3
Cole Fortner <colefortner@gmail.com>
2022-03-19 12:47:59 -0400

    Clean up artifacts that won't be used and organize NavLinks imports

--- src/shared/components/navigation/NavLinks.tsx
+++ src/shared/components/navigation/NavLinks.tsx
@@ -1,41 +1,82 @@
 import { NavLink, useLocation } from "react-router-dom";
 import { useContext } from "react";
-import { AuthContext } from "../../context/auth-context";
-import AdoptionSwiperCube from "../../components/adoption-cube/AdoptionSwiperCube";
 import styled from "styled-components";
 
-const LinkList = styled.ul`
-  display: flex;
-  list-style: none;
-  margin-top: 30px;
+import { AuthContext } from "../../context/auth-context";
+import { ThemeContext } from "../../context/theme-context";
+
+import AdoptionSwiperCube from "../../components/adoption-cube/AdoptionSwiperCube";
+
+const TitleContainer = styled.div`
+  position: absolute;
+  margin-top: 50px;
+  background-color: rgba(0, 0, 0, 0.5);
+  height: 300px;
+  width: 700px;
+  border-radius: 100%;
+  transform: rotate(-2.3deg);
+  text-align: center;
   margin-left: 20px;
+  border: 1px solid white;
+`;
+
+const Title = styled.h1`
+
+  position: absolute;
+  color: white;
+  margin-left: 70px;
+  margin-top: 50px;
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size 100px;
+  padding: 5px;
+  border-radius: 100%;
+`;
+
+const SubTitle = styled.h2`
+  position: absolute;
+  color: white;
+  margin-top: 160px;
+  margin-left: 38px;
+  font-weight: 200;
+  font-family: ${(props) => props.theme.fonts.fancy};
+  font-size: 50px;
+  // transform: rotate(-2.3deg);
+`;
+
+const ListContainer = styled.div`
+  text-align: right;
+  width: 100%;
+  height: 75px;
+  padding: 50px;
+`;
+const LinkList = styled.ul`
+  list-style: none;
+  display: inline-block;
   font-size: 30px;
+  height: 75px;
   a {
     text-decoration: none;
     color: white;
     margin: 10px;
+    &:hover {
+      color: #000;
+    }
   }
 `;
 
 const ListItem = styled.li`
   color: #fff;
-  // background: #333;
   background-color: rgba(0, 0, 0, 0.5);
   border-radius: 4px;
   padding: 20px;
-  margin-left: 4%;
   font-size: 25px;
   cursor: pointer;
-  margin-top: 40px;
   transition: ease-in-out 0.1s;
   border: 1px solid #fff;
   float: right;
   height: 75px;
-
   &:hover {
     background: rgba(255, 255, 255, 0.4);
-
-    // background: #fff;
     color: #000;
     border: 1px solid #fff;
   }
@@ -47,8 +88,8 @@ const Button = styled.button`
 
 const NavLinks: React.FC = () => {
   const auth = useContext(AuthContext);
+  const theme = useContext(ThemeContext);
   const location = useLocation();
-  // console.log(auth.isLoggedIn);
 
   const isCurrentURL = (url: string) => {
     return location.pathname.toLowerCase() === url.toLowerCase();
@@ -56,32 +97,35 @@ const NavLinks: React.FC = () => {
 
   return (
     <>
-      {/* <AdoptionSwiperCube /> */}
-      <LinkList>
-        {!isCurrentURL("/") && (
-          <ListItem>
-            <NavLink to="/">Home</NavLink>
-          </ListItem>
-        )}
-        {/* <li>
-          <NavLink to="/businesses">All businesses</NavLink>
-        </li> */}
-        {auth.isLoggedIn && !isCurrentURL("/users") && (
-          <ListItem>
-            {auth.isLoggedIn && <NavLink to="/users">Dashboard</NavLink>}
-          </ListItem>
-        )}
-        {!auth.isLoggedIn && (
-          <ListItem>
-            <NavLink to="/auth">Login/Signup</NavLink>
-          </ListItem>
-        )}
-        {auth.isLoggedIn && (
-          <ListItem>
-            <Button onClick={auth.logout}>Logout</Button>
-          </ListItem>
-        )}
-      </LinkList>
+      <ListContainer>
+        {/* <AdoptionSwiperCube /> */}
+        <LinkList>
+          {!isCurrentURL("/") && (
+            <ListItem>
+              <NavLink to="/">Home</NavLink>
+            </ListItem>
+          )}
+          {auth.isLoggedIn && !isCurrentURL("/users") && (
+            <ListItem>
+              {auth.isLoggedIn && <NavLink to="/users">Dashboard</NavLink>}
+            </ListItem>
+          )}
+          {!auth.isLoggedIn && (
+            <ListItem>
+              <NavLink to="/auth">Login/Signup</NavLink>
+            </ListItem>
+          )}
+          {auth.isLoggedIn && (
+            <ListItem>
+              <Button onClick={auth.logout}>Logout</Button>
+            </ListItem>
+          )}
+        </LinkList>
+      </ListContainer>
+      <TitleContainer>
+        <Title>Sasha's List</Title>
+        <SubTitle>Your Guide to Dogfriendly St. Pete</SubTitle>
+      </TitleContainer>
     </>
   );
 };
