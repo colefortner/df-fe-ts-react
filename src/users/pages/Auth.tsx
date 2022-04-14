bd86102c6984e43d204b2efc34d88c44bd505538
Cole Fortner <colefortner@gmail.com>
2022-04-13 23:42:54 -0400

    Add basic style to login and sign up form + avatar picker

--- src/users/pages/Auth.tsx
+++ src/users/pages/Auth.tsx
@@ -1,7 +1,67 @@
 import React, { useEffect, useState, useContext } from "react";
-import { NavigationType, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 import { AuthContext } from "../../shared/context/auth-context";
+
+const FormComponentContainer = styled.div`
+  text-align: center;
+`;
+
+const FormContainer = styled.div`
+  display: inline-block;
+  border: 1px solid black;
+  border-radius: 8px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  padding: 15px;
+
+  h2 {
+    text-align: center;
+    margin-bottom: 20px;
+    font-size: 35px;
+  }
+
+  form {
+    text-align: left;
+  }
+
+  input {
+    margin-bottom: 10px;
+    font-size: 20px;
+  }
+
+  label {
+    margin-right: 10px;
+    margin-left: 20px;
+    font-size: 20px;
+    font-weight: bold;
+  }
+
+  button {
+    text-align: center;
+    font-size: 20px;
+    padding: 5px;
+    border-radius: 8px;
+    margin-top: 10px;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  text-align: center;
+`;
+
+const Avatar = styled.img`
+  width: 200px;
+  height: 200px;
+  border-radius: 100px;
+  object-fit: cover;
+`;
+
+const AvatarContainer = styled.div`
+  text-align: center;
+  margin: 10px;
+`;
 
 const Auth: React.FC = () => {
   const auth = useContext(AuthContext);
@@ -107,84 +167,90 @@ const Auth: React.FC = () => {
   };
 
   return (
-    <>
-      <h2>{loginMode ? "Login" : "Signup"}</h2>
-      <form onSubmit={submitHandler}>
-        {!loginMode && (
+    <FormComponentContainer>
+      <FormContainer>
+        <h2>{loginMode ? "Login" : "Signup"}</h2>
+        <form onSubmit={submitHandler}>
+          {!loginMode && (
+            <div>
+              <label htmlFor="username">Username</label>
+              <input
+                id="username"
+                type="text"
+                placeholder="username"
+                name="username"
+                onChange={changeHandler}
+                value={formState.username}
+                required
+              />
+            </div>
+          )}
+          {!loginMode && (
+            <div>
+              <label htmlFor="image">Upload Avatar</label>
+              <input
+                id="image"
+                type="file"
+                multiple={false}
+                accept=".jpg,.png,.jpeg"
+                name="image"
+                onChange={avatarChangeHandler}
+                ref={filePickerRef}
+                // value={formState.image}
+                required
+              />
+              <AvatarContainer>
+                {preview && <Avatar src={preview} alt="avatar preview" />}
+              </AvatarContainer>
+            </div>
+          )}
           <div>
-            <label htmlFor="username">Username</label>
+            <label htmlFor="email">Email Address</label>
             <input
-              id="username"
-              type="text"
-              placeholder="username"
-              name="username"
+              id="email"
+              type="email"
+              placeholder="email"
+              name="email"
               onChange={changeHandler}
-              value={formState.username}
+              value={formState.email}
               required
             />
           </div>
-        )}
-        {!loginMode && (
           <div>
-            <label htmlFor="image">Upload Avatar</label>
+            <label htmlFor="password">Password</label>
             <input
-              id="image"
-              type="file"
-              multiple={false}
-              accept=".jpg,.png,.jpeg"
-              name="image"
-              onChange={avatarChangeHandler}
-              ref={filePickerRef}
-              // value={formState.image}
-              required
-            />
-            <div>{preview && <img src={preview} alt="avatar preview" />}</div>
-          </div>
-        )}
-        <div>
-          <label htmlFor="email">Email Address</label>
-          <input
-            id="email"
-            type="email"
-            placeholder="email"
-            name="email"
-            onChange={changeHandler}
-            value={formState.email}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            placeholder="password"
-            name="password"
-            onChange={changeHandler}
-            value={formState.password}
-            required
-          />
-        </div>
-        {!loginMode && (
-          <div>
-            <label htmlFor="confirm-password">Confirm Password</label>
-            <input
-              id="confirm-password"
+              id="password"
               type="password"
-              placeholder="confirm password"
-              name="confirmPassword"
+              placeholder="password"
+              name="password"
               onChange={changeHandler}
-              value={formState.confirmPassword}
+              value={formState.password}
               required
             />
           </div>
-        )}
-        <button type="submit">{loginMode ? "Login" : "Signup"}</button>
-      </form>
-      <button onClick={changeModeHandler}>
-        Switch to {!loginMode ? "Login" : "Signup"}
-      </button>
-    </>
+          {!loginMode && (
+            <div>
+              <label htmlFor="confirm-password">Confirm Password</label>
+              <input
+                id="confirm-password"
+                type="password"
+                placeholder="confirm password"
+                name="confirmPassword"
+                onChange={changeHandler}
+                value={formState.confirmPassword}
+                required
+              />
+            </div>
+          )}
+          <ButtonWrapper>
+            <button type="submit">{loginMode ? "Login" : "Signup"}</button>
+          </ButtonWrapper>
+        </form>
+        <button onClick={changeModeHandler}>
+          Switch to {!loginMode ? "Login" : "Signup"}
+        </button>
+      </FormContainer>
+    </FormComponentContainer>
   );
 };
 
